865837d98578c9d4f0b2e460a407f26f4104521b
kevinlishuai <kevinlishuai@163.com>
2016-01-02 10:46:41 +0800

    完善publishViewController
    添加注释和一些功能

--- mogujie/mogujie/Classes/Publish/Controller/LSPublishViewController.m
+++ mogujie/mogujie/Classes/Publish/Controller/LSPublishViewController.m
@@ -19,7 +19,7 @@
 
 @implementation LSPublishViewController
 
-
+#pragma mark - 初始化
 - (instancetype)init{
     if (self = [super init]) {
         [self addChildView];
@@ -27,10 +27,22 @@
     return self;
 }
 
+#pragma mark - 添加子控件
 - (void)addChildView{
     LSBottomView * bottomV = [[LSBottomView alloc] init];
     [self.view addSubview:bottomV];
     self.bottomV = bottomV;
+}
+
+#pragma mark - life cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 初始化导航栏
+    [self setupNav];
+
+    // 添加通知观察者
+    [self addObservers];
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -38,8 +50,20 @@
     self.view.backgroundColor = [UIColor lightGrayColor];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+
+#pragma mark - 布局子控件
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    CGFloat h = 300;
+    CGFloat w = [UIScreen mainScreen].bounds.size.width;
+    CGFloat x = 0;
+    CGFloat y = [UIScreen mainScreen].bounds.size.height - h;
+    self.bottomV.frame = CGRectMake(x, y, w, h);
+}
+
+#pragma mark - 初始化导航栏
+- (void)setupNav
+{
     
     UINavigationBar * navBar = [UINavigationBar appearance];
     [navBar setBarTintColor:[UIColor blackColor]];
@@ -50,14 +74,17 @@
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"下一步" style:UIBarButtonItemStyleDone target:self action:@selector(next:)];
     
     [self.navigationItem.rightBarButtonItem setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]} forState:UIControlStateNormal];
-    
+}
+
+#pragma mark - 添加通知观察者
+- (void)addObservers
+{
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(openCamera) name:kOpenCameraNotice object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(choosePhoto:) name:kOpenPhotoNotice object:nil];
-    
 }
 
 
-
+#pragma mark - 监听导航栏按钮的点击
 - (void)back:(UIBarButtonItem *)btn{
     [self dismissViewControllerAnimated:YES completion:nil];
     UINavigationBar * navBar = [UINavigationBar appearance];
@@ -69,26 +96,26 @@
     
 }
 
+
+#pragma mark - note
+/**
+ *  开启相机
+ */
 - (void)openCamera{
     UIImagePickerController * pickerC = [[UIImagePickerController alloc] init];
     pickerC.sourceType = UIImagePickerControllerSourceTypeCamera;
     [self presentViewController:pickerC animated:YES completion:nil];
 }
 
-
+/**
+ *  选择照片
+ *
+ *  @param notification 通知
+ */
 - (void)choosePhoto:(NSNotification *)notification{
     
     NSLog(@"%@", notification.userInfo);
     
-}
-
-- (void)viewDidLayoutSubviews{
-    [super viewDidLayoutSubviews];
-    CGFloat h = 300;
-    CGFloat w = [UIScreen mainScreen].bounds.size.width;
-    CGFloat x = 0;
-    CGFloat y = [UIScreen mainScreen].bounds.size.height - h;
-    self.bottomV.frame = CGRectMake(x, y, w, h);
 }
 
 
